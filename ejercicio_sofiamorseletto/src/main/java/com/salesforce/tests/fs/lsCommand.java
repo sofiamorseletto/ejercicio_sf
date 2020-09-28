b8d0e16df9a6dcce6ef42c74cbeecd4bc2dae183
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2020-09-28 00:19:01 -0300

    Cleaning comments and fixing execution order

--- ejercicio_sofiamorseletto/src/main/java/com/salesforce/tests/fs/lsCommand.java
+++ ejercicio_sofiamorseletto/src/main/java/com/salesforce/tests/fs/lsCommand.java
@@ -6,19 +6,15 @@ public class lsCommand implements Command {
 
     public Component execute(Component component, String parameter) {
         Set<String> names = component.getChildrenNames();
-        String directories[] = parameter.split("/");
-        if(parameter.equals("-r")){
-            //I should execute pwdCommand but I don't have enough time to do the corresponding changes
-            for(String name : names){
-                System.out.println(name);
-            }
-            if(names.size() != 0){
-                for (String name: names){
-                    try{
-                        execute(component.goToChild(name), parameter);
-                    } catch (DirectoryNotFoundException e){
+        for (String name : names) {
+            System.out.println(name);
+        }
+        if (parameter.equals("-r") && names.size() != 0) {
+            for (String name : names) {
+                try {
+                    execute(component.goToChild(name), parameter);
+                } catch (DirectoryNotFoundException e) {
 
-                    }
                 }
             }
         }
