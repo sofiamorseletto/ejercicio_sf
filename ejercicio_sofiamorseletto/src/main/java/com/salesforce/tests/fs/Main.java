2985264a5ca9c91fc7899793a26c9d728f139368
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2020-10-05 13:34:04 -0300

    Extras

--- ejercicio_sofiamorseletto/src/main/java/com/salesforce/tests/fs/Main.java
+++ ejercicio_sofiamorseletto/src/main/java/com/salesforce/tests/fs/Main.java
@@ -1,5 +1,6 @@
 package com.salesforce.tests.fs;
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -14,14 +15,21 @@ public class Main {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
         HashMap<String, Command> commands = setCommands();
         Component actualComponent = new Directory("root", null);
+        try {
+            java.io.File previousState = new java.io.File("archivo.txt");
+            Scanner fileScanner = new Scanner(previousState);
+            String tokens[];
+            while (fileScanner.hasNextLine()) {
+                tokens = fileScanner.nextLine().split(" ");
+                actualComponent = executeCommands(commands, tokens, actualComponent);
+            }
+        } catch (FileNotFoundException e) {
+        }
+
         Scanner scanner = new Scanner(System.in);
         String tokens[] = scanner.nextLine().split(" ");
         while (!tokens[0].equals(quit)) {
-            String parameter = tokens.length > 1 ? tokens[1] : "";
-            if(parameter.equals("-r")){
-                commands.get(("pwd")).execute(actualComponent, parameter);
-            }
-            actualComponent = commands.get(tokens[0]).execute(actualComponent, parameter);
+            actualComponent = executeCommands(commands, tokens, actualComponent);
             tokens = scanner.nextLine().split(" ");
         }
     }
@@ -34,5 +42,13 @@ public class Main {
         commands.put("cd", new cdCommand());
         commands.put("touch", new touchCommand());
         return commands;
+    }
+
+    private static Component executeCommands(HashMap<String, Command> commands,String[] tokens, Component actualComponent) {
+        String parameter = tokens.length > 1 ? tokens[1] : "";
+        if (tokens[0].equals("ls") && !parameter.equals("")) {
+            actualComponent = parameter.equals("-r") ? commands.get(("pwd")).execute(actualComponent, parameter) : commands.get(("cd")).execute(actualComponent, parameter);
+        }
+        return commands.get(tokens[0]).execute(actualComponent, parameter);
     }
 }
